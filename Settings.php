f72f9ce3be309e4a77a7907575900cb2dbdc6546
Vasil Sokolov <vasil@afterlogic.com>
2023-08-01 18:45:20 +0200

    refactored module settings

---
+++ Settings.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * This code is licensed under AGPLv3 license or Afterlogic Software License
+ * if commercial version of the product was purchased.
+ * For full statements of the licenses see LICENSE-AFTERLOGIC and LICENSE-AGPL3 files.
+ */
+
+namespace Aurora\Modules\LdapChangePasswordPlugin;
+
+use Aurora\System\SettingsProperty;
+
+/**
+ * @property bool $Disabled"
+ * @property string $AuthServerUrl"
+ * @property string $Realm"
+ * @property string $ClientId"
+ * @property string $clientSecret"
+ */
+
+class Settings extends \Aurora\System\Module\Settings
+{
+    protected function initDefaults()
+    {
+        $this->aContainer = [
+            "Disabled" => new SettingsProperty(
+                false,
+                "bool",
+                null,
+                ""
+            ),
+            "AuthServerUrl" => new SettingsProperty(
+                "",
+                "string",
+                null,
+                ""
+            ),
+            "Realm" => new SettingsProperty(
+                "",
+                "string",
+                null,
+                ""
+            ),
+            "ClientId" => new SettingsProperty(
+                "",
+                "string",
+                null,
+                ""
+            ),
+            "clientSecret" => new SettingsProperty(
+                "",
+                "string",
+                null,
+                ""
+            ),
+        ];
+    }
+}
